696a60e0582e2e2f093f1eae20d3b2f1f2e4a1a4
poiso <poiso@DESKTOP-D0VR4OM>
2018-08-09 17:37:07 +0900

    hh

---
+++ git/src/git/gg.java
@@ -0,0 +1,14 @@
+package git;
+
+public class gg {
+	public static void main(String[] args) {
+		String a ;
+		String b;
+		a = "안녕하세요";
+		b = "안녕하십니까?";
+		
+		
+		System.out.println(a);
+		
+	}
+}
