f2790e82ec5d3601518fc48071a21f2e2e177357
poiso <poiso@DESKTOP-D0VR4OM>
2018-08-10 10:38:44 +0900

    hihi

---
+++ git/src/git/gg1.java
@@ -0,0 +1,9 @@
+package git;
+
+public class gg1 {
+	public static void mian(String[] args) {
+		String a = "Αρ°ΕΏξ";
+		int c = 5;
+		
+	}
+}
---
+++ git/src/git/ggyoyo.java
@@ -0,0 +1,14 @@
+package git;
+
+public class ggyoyo {
+
+	public static void main(String[] args) {
+		String a= "¾È";
+		String b = "³ç";
+		String c = "ÇÏ";
+		String d = "¼¼";
+		String e = "¿ä";
+		
+		
+	}
+}
